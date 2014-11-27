210d318d3dfb593afddf8e4bdc8e3f656d720ba4
Joel Hermanns <joel.hermanns@gmail.com>
2014-11-27 09:29:39 +0100

    Added source

---
+++ Main.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Monoid
+import Data.Conduit.Binary (sinkFile)
+import qualified Data.Conduit as C
+import Network.HTTP.Conduit
+import Data.Maybe
+import Control.Applicative ((<$>))
+import Control.Monad.IO.Class
+import System.Environment
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy as LBS
+
+import Web.Scotty
+
+fromUrl :: String -> Request
+fromUrl = fromJust . parseUrl
+
+getCalendar :: IO LBS.ByteString
+getCalendar = do
+  user <- BS.pack <$> getEnv "CAMPUS_USER"
+  password <- BS.pack <$> getEnv "CAMPUS_PASS"
+  let iCalReq = fromUrl "https://www.campus.rwth-aachen.de/office/views/calendar/iCalExport.asp?startdt=01.10.2014&enddt=31.03.2015%2023:59:59"
+      authReq = (fromUrl "https://www.campus.rwth-aachen.de/office/views/campus/redirect.asp") {
+        method = "POST",
+        queryString = "?u=" <> user <> "&p=" <> password <> "&login=>%20Login"
+        }
+      initialReq = fromUrl "https://www.campus.rwth-aachen.de/office/"
+  withManager $ \mgr -> do
+    initialResp <- httpLbs initialReq mgr
+    let cookie = responseCookieJar initialResp
+    _ <- httpLbs (authReq { cookieJar = Just cookie}) mgr
+    icalResp <- httpLbs (iCalReq { cookieJar = Just cookie}) mgr
+    return $ responseBody icalResp
+
+main :: IO ()
+main = do
+  port <- read <$> getEnv "PORT"
+  scotty port $ do
+    get "/" $ do
+        setHeader "Content" "text/calendar"
+        setHeader "Content-Disposition" "attachment; filename=calendar.ics"
+        cal <- liftIO $ getCalendar
+        raw cal
